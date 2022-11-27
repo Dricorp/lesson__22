cab6a41a49d5f286d4bf9e70ca0c1ca32ae25269
Dricorp <113799720+Dricorp@users.noreply.github.com>
2022-11-27 14:00:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Строгий режим
+"use strict"
+
+
+let colorIceUser;
+colorIceUser = "Blue";
+console.log(colorIceUser);
+/*
+let user;
+let userName;
+userName = "Vasya";
+user = userName;
+console.log(user);
+*/
+let userAge = 36;
+let userInfo = "Freeelancer ${userAge}";
+console.log(userAge);
+
+
+let userHeight = 148 / 0;
+console.log(userHeight);
+
+let userName;
+console.log(typeof userName);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
